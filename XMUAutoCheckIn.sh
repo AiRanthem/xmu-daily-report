1b4160957b8efb53e576f6244d36f3b7ca3e37be
PhoenixLjw <PhoenixLjw@outlook.com>
2020-12-04 00:00:42 +0800

    init

--- XMUAutoCheckIn.sh
+++ XMUAutoCheckIn.sh
@@ -1,5 +1,4 @@
 #！/bin/bash
-#source /home/sigurd/miniconda3/bin/activate
 Xvfb :99 -ac -screen 0 1280x1024x24 & 
 export DISPLAY=:99
-python3 /home/sigurd/Scripts/XMUAutoCheckIn/XMUAutoCheckIn.py
+python3 XMUAutoCheckIn.py
