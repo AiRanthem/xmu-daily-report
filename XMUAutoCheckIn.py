bc3d2d45df72e0f6abeb46f1e1cebcc5dcbb5260
Jason Lu <49148168+PhoenixLjw@users.noreply.github.com>
2020-12-04 13:43:56 +0800

    Update XMUAutoCheckIn.py

--- XMUAutoCheckIn.py
+++ XMUAutoCheckIn.py
@@ -147,7 +147,7 @@ def sendMail(from_addr, mail_pwd, to_addr, smtp_server, output):
 
     server = smtplib.SMTP_SSL(smtp_server, 465)
     server.set_debuglevel(1)
-    server.login(from_addr, password)
+    server.login(from_addr, mail_pwd)
     logger.info("Mail Login Success")
     server.sendmail(from_addr, [to_addr], msg.as_string())
     logger.info("Send Mail Success")
