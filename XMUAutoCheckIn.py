0985866b523549d4afefd272eb4bcc1dea8c45fa
Ephraim <49148168+PhoenixLjw@users.noreply.github.com>
2021-03-30 11:19:49 +0800

    Update XMUAutoCheckIn.py

--- XMUAutoCheckIn.py
+++ XMUAutoCheckIn.py
@@ -73,7 +73,7 @@ def checkin():
     login.click()
 
     # 选择统一身份认证登录跳转到真正的登录页面
-    login = driver.find_element_by_xpath("//*[@class='buttonBox']/button[2]")
+    login = driver.find_element_by_xpath("//*[@id="loginLayout"]/div[3]/div[2]/div/button[2]")
     login.click()
 
     # 查找页面元素，如果某些元素查找不到则返回错误
